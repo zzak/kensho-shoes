382a3f32e63d1bfa61da6b31c104bdaa3e7b2e3c
zzak <zachary.s.scott@gmail.com>
2010-12-13 18:52:05 -0500

    initial form

--- kensho.rb
+++ kensho.rb
@@ -2,7 +2,25 @@ require 'rubygems'
 require 'bundler'
 Bundler.setup
 require 'green_shoes'
+require 'w3c_validators'
+require 'nokogiri'
 
 Shoes.app do
-  para "Hello, kensho!"
+  stack do 
+    flow do 
+      para "kensho" 
+    end
+
+    flow do
+      lb = list_box items: ["HTML", "CSS", "XML"], choose: "HTML" do |item|
+        @type = item 
+      end
+    end
+
+    flow do
+      eb = edit_box do
+        
+      end
+    end
+  end
 end
