9a21a9ed302f0070410fb7a3b361177317551262
zzak <zachary.s.scott@gmail.com>
2010-12-13 19:23:11 -0500

    setting up kensho as a url type shoes app

--- kensho.rb
+++ kensho.rb
@@ -5,22 +5,40 @@ require 'green_shoes'
 require 'w3c_validators'
 require 'nokogiri'
 
-Shoes.app do
-  stack do 
-    flow do 
-      para "kensho" 
-    end
+class Kensho < Shoes
+  url '/', :index
+  url '/validate', :validate
 
-    flow do
-      lb = list_box items: ["HTML", "CSS", "XML"], choose: "HTML" do |item|
-        @type = item 
+  def index
+    stack do
+      flow do
+        para "kensho"
       end
-    end
 
-    flow do
-      eb = edit_box do
-        
+      flow do
+        list_box items: ['HTML', 'CSS', 'XML'], choose: 'HTML' do |lb|
+          @type = lb
+        end
+      end
+
+      flow do
+        edit_box do |eb| 
+          @markup = eb
+        end
+      end
+      
+      flow do
+        button('Validate') do
+          visit '/validate'
+        end 
       end
     end
   end
+
+  def validate
+    para @type.text
+    para @markup.text
+  end
 end
+
+Shoes.app width: 400, height: 400, title: 'kensho'
